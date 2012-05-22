497f614865c817165c574dd28022bffe0b537136
Vishal Bhoj <vishal.bhoj@linaro.org>
2012-05-22 14:55:55 +0530

    system tests: Add test for loacal audio and video playback
    Change-Id: Icf884b4cf0c542907b92b6cc2183bc708ba0341a
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ localswvideo/run.py
@@ -0,0 +1,55 @@
+import os,sys,time
+from subprocess import call
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+
+
+COUNT = 0;
+
+def gen_png_no():
+	global COUNT
+	COUNT = COUNT + 1
+	return str(COUNT)
+
+def take_snapshot(device):
+	result = device.takeSnapshot()
+	result.writeToFile(os.path.realpath(os.path.dirname(__file__))+"/localswvideo_" + gen_png_no() +".png","png")
+
+def init():
+	os.system("rm" + os.path.realpath(os.path.dirname(__file__)) + "/*.png 2> /dev/null")
+
+def main():
+
+	if len(sys.argv) < 2:
+		print "please input the device id"
+		sys.exit(0)
+
+	#connect the device
+	global device
+	timeout_val = 5
+	for dev_id in sys.argv[1:]:
+		device = MonkeyRunner.waitForConnection( timeout = timeout_val,deviceId = dev_id )
+
+                if not device:
+			print "connect to the device timeout"
+			sys.exit(0)
+		else:
+			print "connect to the device: " + dev_id
+
+		srcfile = os.path.realpath(os.path.dirname(__file__))+'/big_buck_bunny_480p_H264_AAC_25fps_1800K_short.MP4'
+		device.shell("mkdir /data/local/tests/")
+		call(["adb", "-s", dev_id, "push", srcfile, "/data/local/tests/"])
+
+		#clean the enviroment
+		init()
+
+                device.startActivity(uri='file:///data/local/tests/big_buck_bunny_480p_H264_AAC_25fps_1800K_short.MP4',component='com.android.gallery3d/.app.MovieActivity')
+
+		MonkeyRunner.sleep(2)
+		take_snapshot(device)
+
+		device.press("KEYCODE_HOME", MonkeyDevice.DOWN_AND_UP)
+		take_snapshot(device)
+		device.shell("rm -r /data/local/tests/")
+
+if __name__ == '__main__':
+	main()
